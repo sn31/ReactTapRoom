0606eb1eac0f9a81c3267afe51f0c25178142e87
SkyeN <skye@dames.es>
2018-11-16 14:48:44 -0800

    add about component

---
+++ src/components/About.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1><hr/>
+      <style jsx>{`
+        div {
+          text-align: center;
+          width: 500px;
+          background-color: rgba(144, 148, 155, 0.8);
+          padding: 1em;
+        }
+      `}</style>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed augue
+        magna, sagittis sed eleifend eu, mattis nec elit. Fusce sit amet rutrum
+        sapien. Proin blandit maximus arcu, rhoncus ultricies mi ullamcorper in.
+        Duis nec velit arcu. Fusce in dui et ex dapibus malesuada at vitae odio.
+        Nullam pretium varius leo quis accumsan. Suspendisse potenti. Ut turpis
+        mauris, consectetur vitae lorem ac, aliquam consequat ligula.
+      </p>
+
+      <p>
+        Suspendisse potenti. Vivamus nulla lacus, pharetra in blandit ac,
+        vulputate quis tellus. Vivamus id eros eros. Quisque id tortor dolor.
+        Quisque massa ipsum, venenatis ac nisi vitae, pulvinar vestibulum
+        turpis. Nullam dui nunc, fringilla id lorem id, tempus maximus nisl. In
+        id posuere eros. Sed vehicula at tortor quis interdum. Curabitur
+        tincidunt scelerisque gravida. Maecenas pharetra enim massa, at ornare
+        diam aliquet at. Aliquam sit amet feugiat arcu. Vivamus sagittis varius
+        ante et rhoncus. Fusce vitae sagittis odio. Cras eu mi ac odio tincidunt
+        feugiat vitae eu elit.
+      </p>
+      <p>
+        Donec metus risus, mollis eget sapien in, ornare bibendum felis. Ut
+        rhoncus commodo est eu mollis. Quisque eu tempus nisl. Morbi malesuada
+        pharetra enim nec vulputate. Fusce ut fringilla dolor. Sed dui velit,
+        mattis eu gravida non, mattis nec velit. Vestibulum ante ipsum primis in
+        faucibus orci luctus et ultrices posuere cubilia Curae; Praesent quis
+        tincidunt mauris. Donec rhoncus enim ut efficitur auctor. Nulla mollis
+        rhoncus lectus eu finibus. Vivamus ornare in ipsum ac faucibus.
+        Suspendisse tincidunt commodo purus, eget egestas diam malesuada eget.
+      </p>
+      <p>
+        Sed tempor suscipit turpis a dictum. Aliquam rutrum nisl vel dolor
+        euismod, at euismod ligula consectetur. Pellentesque et lacus urna. Sed
+        imperdiet, mi non feugiat mattis, nulla magna euismod nulla, at
+        vestibulum turpis elit nec arcu. Maecenas sodales velit nec ligula
+        tempus, vitae gravida nulla feugiat. Mauris rhoncus placerat augue, non
+        egestas nibh aliquet vel. Class aptent taciti sociosqu ad litora
+        torquent per conubia nostra, per inceptos himenaeos. Mauris lacinia urna
+        nisl, sit amet scelerisque nisl malesuada vel. Ut dignissim ac elit nec
+        facilisis.
+      </p>
+    </div>
+  );
+}
+
+export default About;
