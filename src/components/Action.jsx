75403eea505b30c3dc96aed32100050d6808d495
SkyeN <skye@dames.es>
2018-11-16 10:19:07 -0800

    style action buttons

--- src/components/Action.jsx
+++ src/components/Action.jsx
@@ -4,7 +4,19 @@ import PropTypes from 'prop-types'
 function Action(props) {
   return(
     <div>
-      <a>{props.action}</a>
+      <a>{props.action}</a><br/>
+      <style jsx>{`
+        a {
+            display: block;
+            border: 1px solid pink;
+            width: 6em;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        div {
+            text-align: center;
+        }
+      `}</style>
     </div>
   )
 }
